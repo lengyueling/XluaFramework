12057e8af6b4223db19f699c441af87c077ce9c7
lengyueling <1030952490@qq.com>
2022-07-12 01:59:46 +0800

    add uigroup

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -7,9 +7,52 @@ using Object = UnityEngine.Object;
 public class UIManager : MonoBehaviour
 {
     /// <summary>
-    /// 缓存UI
+    /// 缓存UI字典
     /// </summary>
     Dictionary<string, GameObject> m_UI = new Dictionary<string, GameObject>();
+    /// <summary>
+    /// UI层级分组字典
+    /// </summary>
+    Dictionary<string, Transform> m_UIGroups = new Dictionary<string, Transform>();
+
+    /// <summary>
+    /// UI根节点
+    /// </summary>
+    private Transform m_UIParent;
+    private void Awake()
+    {
+        m_UIParent = this.transform.parent.Find("UI");
+    }
+
+    /// <summary>
+    /// 设置UI层级分组
+    /// UI分组也需要热更新
+    /// 因此该在lua脚本中被调用
+    /// </summary>
+    /// <param name="group"></param>
+    public void SetUIGroup(List<string> group)
+    {
+        for (int i = 0; i < group.Count; i++)
+        {
+            GameObject go = new GameObject("Group-" + group[i]);
+            //flase意味着不保持世界坐标而是跟随父节点
+            go.transform.SetParent(m_UIParent, false);
+            m_UIGroups.Add(group[i], go.transform);
+        }
+    }
+
+    /// <summary>
+    /// 获取UI分组
+    /// </summary>
+    /// <returns>返回具体的UI分组，作为载入ui的父物体</returns>
+    private Transform GetUIGroup(string group)
+    {
+        if (!m_UIGroups.ContainsKey(group))
+        {
+            Debug.Log("分组不存在");
+        }
+        return m_UIGroups[group];
+    }
 
     /// <summary>
     /// 打开UI
@@ -18,21 +61,26 @@ public class UIManager : MonoBehaviour
     /// </summary>
     /// <param name="uiName">要打开UI资源的名字</param>
     /// <param name="luaName">Lua模拟MonoBehaviour使用的文件</param>
-    public void OpenUI(string uiName,string luaName)
+    /// <param name="group">ui分组名</param>
+    public void OpenUI(string uiName, string group, string luaName)
     {
         GameObject ui = null;
         //如果之前已经缓存（使用）过了这个ui则直接调用，不需要重新加载资源
         if (m_UI.TryGetValue(uiName,out ui))
         {
-            UILogic uILogic = ui.GetComponent<UILogic>();
-            uILogic.OnOpen();
+            UILogic uiLogic = ui.GetComponent<UILogic>();
+            uiLogic.OnOpen();
             return;
         }
-        //资源加载并执行lambda表达式，初始化并执行OnOpen()函数
+        //资源加载并执行lambda表达式
         Manager.Resource.LoadUI(uiName, (Object obj) =>
          {
              ui = Instantiate(obj) as GameObject;
              m_UI.Add(uiName, ui);
+             //设置当前ui的父物体，是各个分组名所在的对象
+             Transform parent = GetUIGroup(group);
+             ui.transform.SetParent(parent, false);
+             //执行初始化 OnOpen()函数
              UILogic uiLogic = ui.AddComponent<UILogic>();
              uiLogic.Init(luaName);
              uiLogic.OnOpen();
